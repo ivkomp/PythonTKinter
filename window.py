ac2e768fe30d8681e72a4432ff92383cd741220a
ivkomp <ivkomp@gmail.com>
2020-07-12 13:47:32 +0200

    Creat window.py create window Title of window Set size window Set resizable() method window.resizable(False, False) Set bitmap for the iconified widget to BITMAP. Return the bitmap if None is given.

--- window.py
+++ window.py
@@ -1,5 +1,23 @@
 from tkinter import *
 
+class Window:
+    def __init__(self, width, height, title="MyWindow", resizable=(False, False), icon=None):
+        self.root = Tk()
+        self.root.title(title)
+        self.root.geometry(f"{width}x{height}+200+200")
+        self.root.resizable(resizable[0], resizable[1])
+        if icon:
+            self.root.iconbitmap(icon)
+
+    def run(self):
+        self.root.mainloop()
+
+
+if __name__ == "__maine__":
+    window = Window(500, 500, "TKINTER")
+    window.run()
+
+"""
 #create window
 window = Tk()
 
@@ -17,3 +35,5 @@ window.resizable(False, False)
 window.iconbitmap("icoTK.ico")
 
 window.mainloop()
+
+"""
